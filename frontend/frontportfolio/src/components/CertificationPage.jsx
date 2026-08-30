cef39194ba1fae369fa6956df256cd64c927e069
Nitinsaini1515 <gamern662@gmail.com>
2026-08-30 19:08:32 +0530

    Add CertificateCard component and update CertificationPage to display certificates

--- frontend/frontportfolio/src/components/CertificationPage.jsx
+++ frontend/frontportfolio/src/components/CertificationPage.jsx
@@ -1,24 +1,55 @@
 import React from 'react'
-
+import CertificateCard from './uiComponents/CertificateCard'
+const certi=[
+  {
+    "image":"image.png",
+    "name":"Participant",
+    "description":"this is my participation certificate",
+  },
+  {
+    "image":"image.png",
+    "name":"Participant",
+    "description":"this is my participation certificate",
+  },
+  {
+    "image":"image.png",
+    "name":"Participant",
+    "description":"this is my participation certificate",
+  },
+  {
+    "image":"image.png",
+    "name":"Participant",
+    "description":"this is my participation certificate",
+  },
+  {
+    "image":"image.png",
+    "name":"Participant",
+    "description":"this is my participation certificate",
+  }
+]
 const CertificationPage = () => {
   return (
     <>
       <div>
 {/* header section */}
-<div className='w-150 h-30 ml-65 mt-40'>
+<div className='w-150  h-30 ml-65 mt-40'>
   <h2 className='font-bold text-xl '>Achievements</h2>
   <h1 className='font-bold text-6xl'>My Certificates</h1>
 </div>
 
-<div>
-<div> cer1</div>
-<div>cer2</div>
-<div>cer3</div>
-<div>cer4</div>
-<div>cer5</div>
-<div>cer6</div>
-<div>cer7</div>
-<div>cer8</div>
+<div className='gap-10 shadow-sm shadow-black/20 flex flex-wrap mt-10 w-350 ml-20'>
+{certi.map((certi,index)=>{
+return(
+<CertificateCard
+key = {index}
+image = {certi.image}
+name = {certi.name}
+description={certi.description}
+/>
+
+)
+
+})}
 
 </div>
       </div>
---
+++ frontend/frontportfolio/src/components/uiComponents/CertificateCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const CertificateCard = ({index,image,name,description}) => {
+  return (
+    <>
+      <div className=' shadow-sm shadow-black/20 rounded-2xl p-4 w-100  ml-10 mt-10 '>
+        <div>
+          <img className='h-50 w-full p-2 ' src={image} name="Certimage" />
+          <label htmlFor="Certimage" id='Certimage'>{name}</label>
+          </div>
+          <div>{description}</div>
+      </div>
+    </>
+  )
+}
+
+export default CertificateCard
+
